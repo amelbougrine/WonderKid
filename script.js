989ff2536a47dfe40e95ef34364cfa153787e14d
amelbougrine <amel.1989ab@gmail.com>
2020-12-07 17:41:10 +0100

    3rd commit

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ sectionLeft.addEventListener("mouseout", () => {
     image2.style.height= "300px";
     image2.style.right= "270px";
     image2.style.bottom= "20px";
-    h1Right.style.fontSize= "50px";
+    h1Right.style.fontSize= "60px";
 });
 sectionRight.addEventListener("mouseout", () => {
     image.style.width= "300px";
     image.style.height= "300px";
     image.style.marginLeft= "270px";
-    h1Left.style.fontSize= "50px";
+    h1Left.style.fontSize= "60px";
 });
