dbb20efc1599e39a95c9fe43b34714d8f6716a27
amelbougrine <amel.1989ab@gmail.com>
2020-12-07 12:07:50 +0100

    2nd commit

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ const image = document.querySelector(".image");
 const image2 = document.querySelector(".image2");
 const sectionLeft= document.querySelector(".section--left");
 const sectionRight= document.querySelector(".section--right");
-
+const h1Left= document.querySelector(".title").firstElementChild;
+const h1Right= document.querySelector(".title2").firstElementChild;
 
 sectionLeft.addEventListener("mouseover", () => {
-    image2.style.width= "200px";
-    image2.style.height= "200px";
+    image2.style.width= "230px";
+    image2.style.height= "230px";
+    image2.style.right= "80px";
+    image2.style.bottom= "40px";
+    h1Right.style.fontSize= "45px";
 });
 sectionRight.addEventListener("mouseover", () => {
-    image.style.width= "200px";
-    image.style.height= "200px";
+    image.style.width= "230px";
+    image.style.height= "230px";
+    image.style.marginLeft= "90px";
+    h1Left.style.fontSize= "45px";
 });
 sectionLeft.addEventListener("mouseout", () => {
     image2.style.width= "300px";
     image2.style.height= "300px";
+    image2.style.right= "270px";
+    image2.style.bottom= "20px";
+    h1Right.style.fontSize= "50px";
 });
 sectionRight.addEventListener("mouseout", () => {
     image.style.width= "300px";
     image.style.height= "300px";
+    image.style.marginLeft= "270px";
+    h1Left.style.fontSize= "50px";
 });
